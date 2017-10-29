5659f7e7a6852e6791cf6f459678551c3fa35761
Walker <walker@praiseweb.com.br>
2017-10-29 17:59:45 -0200

    feat(template): add hello prop

--- template/client/App.vue
+++ template/client/App.vue
@@ -1,9 +1,15 @@
 <template lang="html">
-  <h1>Hello World!</h1>
+  <h1>{{ hello }}</h1>
 </template>
 
 <script>
 export default {
+  props: {
+    hello: {
+      type: String,
+      default: 'Hello World!',
+    }
+  }
 }
 </script>
 
