3e9e45e2f9da62b66e9d6b084c42ff60bcbbd27b
Walker Leite <walker@incuca.com.br>
2017-12-12 12:57:30 -0200

    fix(tests): add vuefy compiler to karma

--- template/test/karma.conf.js
+++ template/test/karma.conf.js
@@ -1,7 +1,20 @@
 import babelify from 'babelify';
 import vueify from 'vueify';
+import { dirs } from '../gulp-tasks/config';
+import { customSass } from '../gulp-tasks/compilers.js';
 
 export default (config) => {
+  vueify.compiler.applyConfig({
+    sass: {
+      includePaths: [
+        dirs.modules,
+      ],
+    },
+    customCompilers: {
+      scss: customSass,
+    },
+  });
+
   config.set({
     browsers: ['Chrome'],
     frameworks: ['browserify', 'mocha', 'chai'],
