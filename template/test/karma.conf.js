0ba8e18eb0943255b63888c3741b686ac485aadb
Walker Leite <walker@incuca.com.br>
2017-12-12 13:41:53 -0200

    fix(tests): add karma modulesify settings

--- template/test/karma.conf.js
+++ template/test/karma.conf.js
@@ -1,7 +1,12 @@
 import babelify from 'babelify';
 import vueify from 'vueify';
+import modulesify from 'css-modulesify';
+import tmp from 'tmp';
 import { dirs } from '../gulp-tasks/config';
 import { customSass } from '../gulp-tasks/compilers.js';
+
+const cssBundleFile = tmp.fileSync();
+const cssStream = fs.createWriteStream(cssBundleFile.name);
 
 export default (config) => {
   vueify.compiler.applyConfig({
@@ -21,9 +26,25 @@ export default (config) => {
     preprocessors: {
       '**/*.js': ['browserify'],
     },
+    files: [
+      cssBundleFile.name,
+    ],
     browserify: {
+      output: cssBundleFile.name,
       debug: true,
       transform: [babelify, vueify],
+      plugin: [[modulesify, {
+        global: true,
+        generateScopedName: function (name, filename) {
+          var matches = filename.match(/^\/node_modules/);
+          if (matches) return name;
+          if (process.env.NODE_ENV === 'production') {
+            return modulesify.generateShortName(name, filename);
+          } else {
+            return modulesify.generateLongName(name, filename);
+          }
+        },
+      }]],
     },
   });
 };
