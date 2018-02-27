df301737e694b6a9aa650acf553ad5fc5293d015
Walker Leite <walker@incuca.com.br>
2018-02-27 18:59:39 -0300

    feat(lint): update settings to airbnb - webpack template based

---
+++ template/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/recommended', 'airbnb-base', 'loopback'],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  globals: {
+    expect: true,
+    assert: true,
+    require: true,
+    request: true
+  },
+  // add your custom rules here
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always',
+      {
+      js: 'never',
+      vue: 'never'
+      }
+    ],
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['error',
+      {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e' // for e.returnvalue
+        ]
+      }
+    ],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
