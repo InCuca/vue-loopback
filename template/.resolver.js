ed7ed68fed6c4a8b943a4873b5e0e30d73fa3684
Walker Leite <walker@incuca.com.br>
2018-02-28 13:52:13 -0300

    feat(lint): add resolve script

---
+++ template/.resolver.js
@@ -0,0 +1,16 @@
+var resolve = require('resolve')
+var path = require('path')
+
+exports.resolve = function (source, file, config) {
+  const root = path.resolve(__dirname) + '/'
+  const clientDir = path.resolve(root, 'client') + '/'
+  source = source.replace('~', root)
+  source = source.replace('@', clientDir)
+
+  if (resolve.isCore(source)) return { found: true, path: null }
+  try {
+    return { found: true, path: resolve.sync(source, opts(file, config)) }
+  } catch (err) {
+    return { found: false }
+  }
+}
