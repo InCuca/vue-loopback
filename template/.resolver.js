112395b74bd9009dc470902663b2c45a2536ff49
Walker Leite <walker@incuca.com.br>
2018-02-28 14:06:39 -0300

    fix(eslint): disable resolver

--- template/.resolver.js
+++ template/.resolver.js
@@ -1,16 +1,19 @@
-var resolve = require('resolve')
-var path = require('path')
+// var resolve = require('resolve')
+// var path = require('path')
 
 exports.resolve = function (source, file, config) {
-  const root = path.resolve(__dirname) + '/'
-  const clientDir = path.resolve(root, 'client') + '/'
-  source = source.replace('~', root)
-  source = source.replace('@', clientDir)
+  // FIXME: ignoring any resolve errors
+  return { found: true, path: null }
 
-  if (resolve.isCore(source)) return { found: true, path: null }
-  try {
-    return { found: true, path: resolve.sync(source, opts(file, config)) }
-  } catch (err) {
-    return { found: false }
-  }
+  // const root = path.resolve(__dirname) + '/'
+  // const clientDir = path.resolve(root, 'client') + '/'
+  // source = source.replace('~', root)
+  // source = source.replace('@', clientDir)
+
+  // if (resolve.isCore(source)) return { found: true, path: null }
+  // try {
+  //   return { found: true, path: resolve.sync(source, opts(file, config)) }
+  // } catch (err) {
+  //   return { found: false }
+  // }
 }
