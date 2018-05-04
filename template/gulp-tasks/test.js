08e5ece0121c9457d19e2aaf8ed6c402cac12099
Walker Leite <walker@incuca.com.br>
2018-05-04 11:34:55 -0300

    feat(gulp): add optional -t parameter to test files

--- template/gulp-tasks/test.js
+++ template/gulp-tasks/test.js
@@ -2,12 +2,13 @@ import gulp from 'gulp';
 import mocha from 'gulp-mocha';
 import {Server} from 'karma';
 import path from 'path';
+import {argv} from 'yargs';
 import {dirs} from './config';
 
 gulp.task(
   'test:server',
   () =>
-    gulp.src(path.resolve(dirs.testServer, '**/*.spec.js'))
+    gulp.src(argv.t || path.resolve(dirs.testServer, '**/*.spec.js'))
       .pipe(mocha({
         compilers: 'js:babel-core/register',
         require: path.resolve(dirs.test, 'mocha.conf.js'),
@@ -16,7 +17,7 @@ gulp.task(
 
 gulp.task('test:client', (done) => {
   new Server({
-    files: [path.resolve(dirs.testClient, '**/*.spec.js')],
+    files: [argv.t || path.resolve(dirs.testClient, '**/*.spec.js')],
     configFile: path.resolve(dirs.test, 'karma.conf.js'),
     singleRun: true,
   }, done).start();
