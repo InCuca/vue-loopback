7907f28f78f5be85f6e3376ef57f9ff771f9bb2b
Walker Leite <walker@incuca.com.br>
2018-05-10 18:54:44 -0300

    fix(karma): throws when vue warns

--- template/test/karma.conf.js
+++ template/test/karma.conf.js
@@ -2,10 +2,26 @@ import babelify from 'babelify';
 import vueify from 'vueify';
 import modulesify from 'css-modulesify';
 import tmp from 'tmp';
+import through from 'through2';
 import {dirs} from '../gulp-tasks/config';
 import {customSass} from '../gulp-tasks/compilers';
 
 const cssBundleFile = tmp.fileSync();
+
+// Just hook window.console to throw vue warn
+const consoleAppendfy = () => through(function (buf, enc, next) {
+  const hook = `
+    const error = console.error;
+    console.error = function(warning, ...args) {
+      if (/(Vue warn)/.test(warning)) {
+        throw new Error(warning);
+      }
+      error.apply(console, [warning, ...args]);
+    };
+  `;
+  this.push(hook + buf.toString('utf8'));
+  next();
+});
 
 export default (config) => {
   vueify.compiler.applyConfig({
@@ -31,7 +47,7 @@ export default (config) => {
     browserify: {
       output: cssBundleFile.name,
       debug: true,
-      transform: [babelify, vueify],
+      transform: [consoleAppendfy, babelify, vueify],
       plugin: [[modulesify, {
         global: true,
         generateScopedName(name, filename) {
