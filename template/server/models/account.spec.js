dafddda60bfdb36333acd50c7d15f0574c370ecc
Walker <walker@praiseweb.com.br>
2018-07-04 11:36:48 -0300

    test(account): improve test coverage

--- template/server/models/account.spec.js
+++ template/server/models/account.spec.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import createLoopback from '~/test/utils/create-loopback';
 import AccountFactory from './account';
-import { host } from '../config.json';
+import {host} from '../config.json';
 
 describe('Account unit', () => {
   const AccountMock = {
@@ -31,6 +31,10 @@ describe('Account unit', () => {
     AccountMock.on.mockImplementation((_, fn) => {
       fn(infoMock);
     });
+    console.log = jest.fn();
+    AccountMock.app.models.Email.send.mockImplementation((_, errFn) => {
+      errFn();
+    });
     AccountFactory(AccountMock);
     expect(AccountMock.app.models.Email.send).toBeCalledWith(
       expect.objectContaining({
@@ -43,10 +47,32 @@ describe('Account unit', () => {
       }),
       expect.any(Function),
     );
+    expect(console.log).toBeCalledWith(
+      expect.any(String),
+      expect.stringContaining(infoMock.email)
+    );
+  });
+
+  it('console log when Email send throws', () => {
+    const infoMock = {
+      email: 'foo@bar.net',
+      accessToken: {
+        id: 'foobar',
+      },
+    };
+    AccountMock.on.mockImplementation((_, fn) => {
+      fn(infoMock);
+    });
+    console.log = jest.fn();
+    AccountMock.app.models.Email.send.mockImplementation((_, errFn) => {
+      errFn('foo');
+    });
+    AccountFactory(AccountMock);
+    expect(console.log).toBeCalledWith('foo');
   });
 });
 
-describe('Account', () => {
+describe('Account e2e', () => {
   const email = '936ue5+4bnywbeje42pw@sharklasers.com';
   let server, testAccount, Account;
 
